1ba5ab7264cfd96a9ab0b5f1b7655a402bb2d32a
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-01-12 19:30:46 +0300

    12.01.22

---
+++ src/day04_test/C04_test.java
@@ -0,0 +1,28 @@
+package day04_test;
+import java.util.Scanner;
+
+public class C04_test {
+    public static  void main(String[] args) {
+        // kullanicidan ismini isteyin
+        // girilen ismi
+        // isminiz : Ercan    seklinde yazdirin
+
+        // insanlarin dunyasindan kod'larimizin bulundugu class'a deger almak icin
+        // Scanner class'ini kullaniriz
+
+        // 1- Scanner objesi olusturalim
+        Scanner Scan = new Scanner(System.in);
+
+        // 2- Kullaniciya ne istedigimizi soyleyelim
+        System.out.println("Lutfen isminizi giriniz");
+
+        // 3- olusturdugumuz scan objesi ile kullanicinin girdigi degeri alip
+        // olusturacagimiz uygun bir variable'a atayalim
+        String kullaniciIsmi=Scan.next();
+        System.out.println("Isminiz: " + kullaniciIsmi);
+
+
+
+
+    }
+}
---
+++ src/day39_/C01.java
@@ -0,0 +1,7 @@
+package day39_;
+
+public class C01 {
+    public static void main(String[] args) {
+
+    }
+}

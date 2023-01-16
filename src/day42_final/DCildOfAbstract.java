40698d057edbb4d82caab77df3f58f89eecd4b12
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-01-16 17:12:28 +0300

    16/01/2023

---
+++ src/day42_final/DCildOfAbstract.java
@@ -0,0 +1,22 @@
+package day42_final;
+
+public class DCildOfAbstract extends CAbstract{
+    @Override
+    void carpma() {
+        /*
+        Abstract class'lar child class'larin
+        sahip olmalari gereken
+        mecburi ozellikleri belirlerler
+        (Gunluk hayatimizdaki standartlar gibi)
+        Abstract bir class'i parent edinen tum child'lar
+        parent abstract class'daki
+        tum abstract methodlari override etmek zorundadirlar
+
+         */
+    }
+
+    @Override
+    void bolme() {
+
+    }
+}

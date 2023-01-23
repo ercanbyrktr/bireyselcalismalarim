dd1b00e87f6f41ea1bfef5a22c77e31038a5205f
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-01-23 17:46:35 +0300

    23/01/2023

--- src/day46_collections/C05_LinkedList_DequeDataTuru.java
+++ src/day46_collections/C05_LinkedList_DequeDataTuru.java
@@ -14,7 +14,14 @@ public class C05_LinkedList_DequeDataTuru {
         ll4.add("Selim");
 
         ll4.removeLastOccurrence("Selim");
+        ll4.removeFirstOccurrence("Hasan");
         System.out.println(ll4); // [Cavidan, Mesud, Selim, Tevfik]
         System.out.println(ll4.pop());
+        // LinkedList'in basindaki elementi silip, bize dondurur
+
+        ll4.remove();
+        ll4.removeFirst();
+        ll4.removeLast();
+        //Deque iki tarafli queue demektir, dolayisiyla her method'un first ve last'i var
     }
 }

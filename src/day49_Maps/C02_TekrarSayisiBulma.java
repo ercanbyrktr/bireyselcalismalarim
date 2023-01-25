4042b2e7f6c4bf5c228509cd4e954ad57a7bdf3e
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-01-25 15:33:28 +0300

    25/01/2023

---
+++ src/day49_Maps/C02_TekrarSayisiBulma.java
@@ -0,0 +1,43 @@
+package day49_Maps;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class C02_TekrarSayisiBulma {
+
+    public static void main(String[] args) {
+
+        // Verilen bir String'deki kullanilan harfleri
+        // ve kullanilan harflerin tekrar sayisini
+        // H=20 seklinde yazdirin
+
+
+        String str= "Heeeellllloooooo Woooooorrrrrllllldddd.";
+        // space'leri saymamasi icin once onlari yok edelim
+        str=str.replaceAll("\\W","");
+
+        String[] harflerArr=str.split("");
+
+        System.out.println(Arrays.toString(harflerArr));
+        // [H, e, e, e, e, l, l, l, l, l, o, o, o, o, o, o, W, o, o, o, o, o, o, r, r, r, r, r, l, l, l, l, l, d, d, d, d]
+        // H=1 e=4 l=10
+        // harfleri key, kullanim adedini value yaparak bir map olusturalim
+        Map<String,Integer> harfKullanimsayilariMap=new HashMap<>();
+        Integer harfKullanimsayisi;
+
+        for (String each: harflerArr
+             ) {
+            if (!harfKullanimsayilariMap.containsKey(each)){
+                harfKullanimsayilariMap.put(each,1);
+            }else{
+                harfKullanimsayisi=harfKullanimsayilariMap.get(each);
+
+                harfKullanimsayilariMap.put(each,++harfKullanimsayisi);
+            }
+
+        }
+        System.out.println(harfKullanimsayilariMap);
+    }
+}
---
+++ src/day49_Maps/C03_Update.java
@@ -0,0 +1,48 @@
+package day49_Maps;
+
+import day48_maps.ReusableMethods;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class C03_Update {
+    public static void main(String[] args) {
+
+        /*
+        map.contains(key) ==>  verdigimiz Key'in map'de olup olmadigini boolean olarak doner
+        map.contains(value) ==> bir butun olarak value'nun map'de olup olmadigini doner
+
+        ONEMLI NOT : map.contains(value) value'nun icindeki bir parcayi bulmada ise yaramaz
+                    eger value icindeki bir parcayi aratmak istiyorsak
+                    map uzerinde manipulation yapmamiz lazim
+
+         map.get(key) ==> verilen key'e ait degeri dondurur
+         */
+
+        Map<Integer,String> sinifListMap= ReusableMethods.mapOlustur();
+        System.out.println(sinifListMap);
+
+        System.out.println(sinifListMap.containsKey(104));// true
+        System.out.println(sinifListMap.containsKey(114));//false
+
+        System.out.println(sinifListMap.containsValue("Ali, Can, JDev"));//true
+
+        System.out.println(sinifListMap.containsValue("JDev"));//false
+
+        // Verilen map'de JDev degerlerini JavaDeveloper olarak degistirelim
+        //Map'lerde replace tum value'yu degistirmek istersek kullanilabilir
+        // kismi degisikliklerde kullanamayiz
+
+        //ReusableMethods.tumValueSiraliYazdir(sinifListMap);
+        Collection<String> valueCollection= sinifListMap.values();
+
+        for (String each: valueCollection
+             ) {
+            each=each.replaceAll("JDev","JavaDeveloper");
+            System.out.println(each);
+
+        }
+        System.out.println(sinifListMap);
+    }
+}
